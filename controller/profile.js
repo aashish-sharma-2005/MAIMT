a2e226ff05c1f0fb434db84d61e9dd14ef3ef95c
Aashish Sharma <aashishpandit112@gmail.com>
2026-04-10 18:51:06 +0530

    profile name set

--- controller/profile.js
+++ controller/profile.js
@@ -1,56 +1,60 @@
 const UserData = require("../models/user")
+const Post = require("../models/posts")
 const upload = require("../config/cloudinary")
 
-async function getProfile(req,res) {
+async function getProfile(req, res) {
     try {
-        if(req.user.id===req.params.id) return res.redirect('/user/profile')
-        const user = await UserData.findOne({_id:req.params.id})
-        const friends = await Promise.all(user.friends.map(i=>UserData.findById(i)))
-        const currentUser = await UserData.findOne({email:req.user.useremail})
-        res.render('home',{
-            page:'profile',
+        if (req.user.id === req.params.id) return res.redirect('/user/profile')
+        const posts = await Post.find({ user: req.params.id }).populate("user", "name profilePic").sort({ createdAt: -1 });
+        const user = await UserData.findOne({ _id: req.params.id })
+        const friends = await Promise.all(user.friends.map(i => UserData.findById(i)))
+        const currentUser = await UserData.findOne({ email: req.user.useremail })
+        res.render('home', {
+            page: 'profile',
             profileUser: user,      // jiski profile dekh rahe ho
             currentUser: currentUser,
-            username:req.user.username,
-            friends:friends
+            username: req.user.username,
+            friends: friends,
+            posts:posts
         })
     } catch (error) {
         console.log(error)
-        res.json({status:false})
+        res.json({ status: false })
     }
 }
-async function getCurrectProfile(req,res) {
+async function getCurrectProfile(req, res) {
     try {
-        const user = await UserData.findOne({email:req.user.useremail})
-        const friends = await Promise.all(user.friends.map(i=>UserData.findById(i)))
-        res.render('home',{page:'currentprofile',username:req.user.username,user:user,currentuser:user,friends:friends})
+        const posts = await Post.find({ user: req.user.id }).sort({ createdAt: -1 });
+        const user = await UserData.findOne({ email: req.user.useremail })
+        const friends = await Promise.all(user.friends.map(i => UserData.findById(i)))
+        res.render('home', { page: 'currentprofile', username: req.user.username, user: user, currentuser: user, friends: friends, posts:posts})
     } catch (error) {
         console.log(error)
-        res.json({status:false})
+        res.json({ status: false })
     }
 }
-async function uploadpic(req,res){
+async function uploadpic(req, res) {
     try {
-        if(req.file){
-            const result = await UserData.updateOne({_id:req.user.id},{$set:{profilePic:req.file.path}})
-            if(result) return res.json({status:true})
-            else return res.json({status:false})
+        if (req.file) {
+            const result = await UserData.updateOne({ _id: req.user.id }, { $set: { profilePic: req.file.path } })
+            if (result) return res.json({ status: true })
+            else return res.json({ status: false })
         }
     } catch (error) {
         console.log(error)
 
     }
 }
-async function uploadcover(req,res){
+async function uploadcover(req, res) {
     try {
-        if(req.file){
-            const result = await UserData.updateOne({_id:req.user.id},{$set:{coverPhoto:req.file.path}})
-            if(result) return res.json({status:true})
-            else return res.json({status:false})
+        if (req.file) {
+            const result = await UserData.updateOne({ _id: req.user.id }, { $set: { coverPhoto: req.file.path } })
+            if (result) return res.json({ status: true })
+            else return res.json({ status: false })
         }
     } catch (error) {
         console.log(error)
 
     }
 }
-module.exports = {getProfile,getCurrectProfile,uploadpic,uploadcover}
+module.exports = { getProfile, getCurrectProfile, uploadpic, uploadcover }
